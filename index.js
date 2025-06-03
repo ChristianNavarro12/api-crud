11f7394e03de7444424a381b03d1faaf59d812e8
ChristianNavarro12 <christiannavarro12003@gmail.com>
2025-06-03 17:16:19 -0600

    cambioxd

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.json({
+    mensaje: "📌 API REST Students",
+    rutas_disponibles: {
+      GET: "/api/...",
+      POST: "/api/...",
+      PUT: "/api/...",
+      DELETE: "/api/..."
+    },
+    nota: "Las rutas están organizadas así debido a que Vercel no soporta Swagger u otras interfaces de documentación por defecto en funciones serverless."
+  });
+});
+
 app.use("/api", require("./api/routes/indexRouter"));
 app.use("/docs", swaggerRouter);
 
