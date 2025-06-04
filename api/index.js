c3326e467459c26ae933672c091a5304f1b442de
ChristianNavarro12 <christiannavarro12003@gmail.com>
2025-06-03 18:20:47 -0600

    fixed

--- api/index.js
+++ api/index.js
@@ -1,26 +1,24 @@
 const express = require('express');
-const serverless = require('serverless-http');
+const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const swaggerRouter = require("./config/swagger");
 
-const app = express();
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/home", (req, res) => res.send("Express on Vercel"));
 app.use("/api", require("./routes/indexRouter"));
 app.use("/docs", swaggerRouter);
 
-// const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 9000;
 
-// app.listen(PORT, () => {
-//   console.log(`🚀 Servidor en el puerto  http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor en el puerto  http://localhost:${PORT}`);
+});
 
 
 module.exports = app;
-module.exports.handler = serverless(app);
--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const express = require('express');
-const serverless = require('serverless-http');
+const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const swaggerRouter = require("./api/config/swagger");
 
-const app = express();
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/home", (req, res) => res.send("Express on Vercel"));
 
 app.use("/api", require("./api/routes/indexRouter"));
 app.use("/docs", swaggerRouter);
 
-// const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 9000;
 
-// app.listen(PORT, () => {
-//   console.log(`🚀 Servidor en el puerto  http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor en el puerto  http://localhost:${PORT}`);
+});
 
 
 module.exports = app;
-module.exports.handler = serverless(app);
